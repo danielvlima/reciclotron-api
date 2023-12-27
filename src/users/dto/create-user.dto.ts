9dbf456a46a2bb7944b63858dd851fea2b6204fb
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-27 20:51:13 -0300

    feat: adição dos objetos relacionados ao user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { UserGenderEnum } from '../enum/user-gender.enum';
+
+export class CreateUserDto {
+  nome: string;
+  cpf: string;
+  email: string;
+  telefone: string;
+  senha: string;
+  dataAniversario: Date;
+  generoUsuario: UserGenderEnum;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { $Enums } from '@prisma/client';
+
+export class User {
+  cpf: string;
+  email: string;
+  telefone: string;
+  nome: string;
+  senha: string;
+  pontos: number;
+  dataAniversario: Date;
+  generoUsuario: $Enums.GeneroUsuario;
+  nivelPrivilegio: $Enums.RegraPriviegio;
+  criadoEm: Date;
+  atualizadoEm: Date | null;
+  codigoRecuperacao: string | null;
+  codigoRecuperacaoCriadoEm: Date | null;
+}
