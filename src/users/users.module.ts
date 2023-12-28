ec67743fc8f02b0befa943adb7634f3c1e1ad700
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-27 22:30:45 -0300

    feat: criação do módulo de cripto

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { CryptoModule } from 'src/crypto/crypto.module';
 
 @Module({
+  imports: [CryptoModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
