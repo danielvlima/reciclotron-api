4816084a5f8995e0d9aad7f1676b31813fb4b21e
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-27 22:36:06 -0300

    feat: criação do gerador de código

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { CodeGeneratorModule } from './code-generator/code-generator.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, CryptoModule],
+  imports: [UsersModule, PrismaModule, CryptoModule, CodeGeneratorModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { CryptoModule } from 'src/crypto/crypto.module';
+import { CodeGeneratorModule } from 'src/code-generator/code-generator.module';
 
 @Module({
-  imports: [CryptoModule],
+  imports: [CryptoModule, CodeGeneratorModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
