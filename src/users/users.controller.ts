51bc0be86f668b0fbdd277cdb33ca30b6661bfd0
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-27 22:53:37 -0300

    feat: implementação do controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,8 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CryptoModule } from 'src/crypto/crypto.module';
+import { CodeGeneratorModule } from 'src/code-generator/code-generator.module';
 
 @Controller()
 export class UsersController {
@@ -13,23 +15,45 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @MessagePattern('findAllUsers')
-  findAll() {
-    return this.usersService.findAll();
-  }
-
   @MessagePattern('findOneUser')
-  findOne(@Payload() id: number) {
-    return this.usersService.findOne(id);
+  findOne(@Payload() email: string, password: string) {
+    return this.usersService.findOne(email).then((user) => {
+      const passwordSplitted = user.senha.split(':');
+      const passwordHashed = CryptoModule.sha256(password, passwordSplitted[1]);
+
+      if (passwordHashed === passwordSplitted[0]) {
+        return user;
+      }
+    });
   }
 
   @MessagePattern('updateUser')
   update(@Payload() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(updateUserDto.id, updateUserDto);
+    return this.usersService.update(updateUserDto.cpf, updateUserDto);
   }
 
   @MessagePattern('removeUser')
-  remove(@Payload() id: number) {
-    return this.usersService.remove(id);
+  remove(@Payload() cpf: string) {
+    return this.usersService.remove(cpf);
+  }
+
+  @MessagePattern('generateCode')
+  createCode(@Payload() cpf: string) {
+    const code = CodeGeneratorModule.new();
+    return this.usersService.updateRecoveryCode(cpf, code);
+  }
+
+  @MessagePattern('checkCode')
+  checkCode(@Payload() cpf: string, code: string) {
+    return this.usersService.findOneWithCpf(cpf).then((user) => {
+      const now = new Date();
+      const limitDate = new Date(user.codigoRecuperacaoCriadoEm!);
+      limitDate.setMinutes(limitDate.getMinutes() + 15);
+
+      if (now.getTime() - limitDate.getTime() < 0) {
+        return user.codigoRecuperacao === code;
+      }
+      return false;
+    });
   }
 }
