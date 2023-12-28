7f5aecde4682686ce4385c0e9f485f414f75cc2f
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-28 00:22:21 -0300

    fix: erro no import do crypto

--- src/crypto/crypto.module.ts
+++ src/crypto/crypto.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import crypto from 'crypto';
+import { createHash, randomBytes } from 'crypto';
 
 @Module({})
 export class CryptoModule {
   static sha256 = (text: string, salt: string) => {
-    return crypto.createHash('sha256').update(text).update(salt).digest('hex');
+    return createHash('sha256').update(text).update(salt).digest('hex');
   };
 
   static salt = () => {
-    return crypto.randomBytes(8).toString('hex');
+    return randomBytes(8).toString('hex');
   };
 }
