33b882f6c827b1b7f1d5afb0a338db27437949f6
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-28 10:09:59 -0300

    refactor: movendo arquivos de endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './endpoints/users/users.module';
 import { PrismaModule } from './shared/modules/prisma/prisma.module';
 import { CryptoModule } from './shared/modules/crypto/crypto.module';
 import { CodeGeneratorModule } from './shared/modules/code-generator/code-generator.module';
