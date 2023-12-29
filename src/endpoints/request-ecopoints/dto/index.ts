c800d822810c95732fb071d63b8e23f9ba55179a
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-28 21:34:19 -0300

    refactor: remoção de objeto não utilizado

--- src/endpoints/request-ecopoints/dto/index.ts
+++ src/endpoints/request-ecopoints/dto/index.ts
@@ -3,11 +3,9 @@ import { CreateRequestEcopointDto } from './create-request-ecopoint.dto';
 import { PaginatedNewEcopointsRequestDto } from './paginated-new-ecopoints-request.dto';
 import { ResponsePaginatedEcopointsRequestDto } from './response-paginated-ecopoints-request.dto';
 import { ResponseRequestEcopointDto } from './response-request-ecopoint.dto';
-import { UpdateRequestEcopointDto } from './update-request-ecopoint.dto';
 
 export {
   CreateRequestEcopointDto,
-  UpdateRequestEcopointDto,
   CancelRequestEcopointDto,
   PaginatedNewEcopointsRequestDto,
   ResponseRequestEcopointDto,
--- src/endpoints/request-ecopoints/dto/update-request-ecopoint.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRequestEcopointDto } from './create-request-ecopoint.dto';
-
-export class UpdateRequestEcopointDto extends PartialType(CreateRequestEcopointDto) {}
