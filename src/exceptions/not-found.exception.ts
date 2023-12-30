1410ab83958184b4c9ccf569cdd7173188e6ba6d
Ricardo Monteiro <rmonteiro@marlin.com.br>
2023-12-30 17:45:48 -0300

    feat: criação de um exception

---
+++ src/exceptions/not-found.exception.ts
@@ -0,0 +1,20 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+import { ResponseFactoryModule } from 'src/shared/modules/response-factory/response-factory.module';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class NotFoundException implements ExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    if (exception.code === 'P2025') {
+      response
+        .status(404)
+        .json(ResponseFactoryModule.generate('Elemento não encontrado'));
+    } else {
+      response.status(500).json(ResponseFactoryModule.generate('Erro interno'));
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundException } from './exceptions/not-found.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalFilters(new NotFoundException());
+  await app.listen(5000);
 }
 bootstrap();
