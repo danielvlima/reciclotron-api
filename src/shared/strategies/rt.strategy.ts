a2b3e4c9287218c270897959cd9380b709726b77
Ricardo Monteiro <rmonteiro@marlin.com.br>
2024-01-17 23:45:53 -0300

    REC-129 Criação de um tipo para o payload

--- src/shared/strategies/rt.strategy.ts
+++ src/shared/strategies/rt.strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { env } from 'process';
+import { JwtPayload } from '../types';
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
@@ -14,7 +15,7 @@ export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
     });
   }
 
-  validate(req: Request, payload: any) {
+  validate(req: Request, payload: JwtPayload) {
     const refreshToken = req
       ?.get('authorization')
       ?.replace('Bearer', '')
