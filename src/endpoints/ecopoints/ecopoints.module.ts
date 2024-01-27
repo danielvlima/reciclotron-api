e5efd528870935dbc6279afa8225264718830816
Ricardo Monteiro <rmonteiro@marlin.com.br>
2024-01-27 19:37:34 -0300

    REC-129 Importação de estratégias

--- src/endpoints/ecopoints/ecopoints.module.ts
+++ src/endpoints/ecopoints/ecopoints.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { EcopointsService } from './ecopoints.service';
 import { EcopointsController } from './ecopoints.controller';
 import { RequestEcopointsService } from '../request-ecopoints/request-ecopoints.service';
+import { UserStrategy, AdminStrategy } from 'src/shared/strategies';
 
 @Module({
   controllers: [EcopointsController],
-  providers: [EcopointsService, RequestEcopointsService],
+  providers: [
+    EcopointsService,
+    RequestEcopointsService,
+    UserStrategy,
+    AdminStrategy,
+  ],
   exports: [EcopointsService],
 })
 export class EcopointsModule {}
