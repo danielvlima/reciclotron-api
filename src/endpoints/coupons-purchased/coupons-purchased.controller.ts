a81d05db383985a4e6bc58912f28251e5299f8da
Ricardo Monteiro <rmonteiro@marlin.com.br>
2024-01-31 22:00:31 -0300

    REC-129 Troca do tipo de chamada

--- src/endpoints/coupons-purchased/coupons-purchased.controller.ts
+++ src/endpoints/coupons-purchased/coupons-purchased.controller.ts
@@ -5,6 +5,7 @@ import {
   HttpCode,
   HttpStatus,
   UseGuards,
+  Get,
 } from '@nestjs/common';
 import { CouponsPurchasedService } from './coupons-purchased.service';
 import { ResponseDto } from 'src/shared/dto/response.dto';
@@ -28,7 +29,7 @@ export class CouponsPurchasedController {
   @UseGuards(UserGuard)
   @Public()
   @HttpCode(HttpStatus.OK)
-  @Post('filters')
+  @Get('filters')
   findAll(@GetCurrentKey() cpf: string): Promise<ResponseDto<string[]>> {
     const dateNow = new Date();
     dateNow.setHours(0, 0, 0, 0);
