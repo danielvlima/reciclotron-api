413119609403e6786f161cd4a9ad6b7d52744960
Ricardo Monteiro <rmonteiro@marlin.com.br>
2024-02-04 19:46:54 -0300

    REC-127 Implementação das funções

--- src/shared/modules/mail/mail.service.ts
+++ src/shared/modules/mail/mail.service.ts
@@ -20,4 +20,72 @@ export class MailService {
       context,
     });
   }
+
+  async sendSignUp(to: string) {
+    return await this.sendMail(
+      to,
+      'Seja bem vindo à plataforma Reciclopontos',
+      'user/sign-up',
+      {},
+    );
+  }
+
+  async sendGoodbye(to: string, name: string) {
+    return await this.sendMail(
+      to,
+      'Seja bem vindo à plataforma Reciclopontos',
+      'user/goodbye',
+      { name },
+    );
+  }
+
+  async sendForgotPassword(to: string, name: string, code: string) {
+    return await this.sendMail(
+      to,
+      'Seu código de recuperação de senha',
+      'user/forgot-password',
+      { name, code },
+    );
+  }
+
+  async sendPasswordUpdated(to: string, name: string) {
+    return await this.sendMail(
+      to,
+      'Sua senha da plataforma Reciclopontos foi alterada',
+      'user/password-updated',
+      { name },
+    );
+  }
+
+  async sendProfileUpdated(to: string, name: string) {
+    return await this.sendMail(
+      to,
+      'Seu perfil da plataforma Reciclopontos foi atualizado',
+      'user/profile-updated',
+      { name },
+    );
+  }
+
+  async sendUserDepositConfirmed(to: string, name: string, value: string) {
+    return await this.sendMail(
+      to,
+      'Seu depósito foi confirmado na plataforma Reciclopontos',
+      'user/deposit-confirmed',
+      { name, value },
+    );
+  }
+
+  async sendUserNewPurchase(
+    to: string,
+    name: string,
+    cupomName: string,
+    partnerName: string,
+  ) {
+    return await this.sendMail(
+      to,
+      'Seu cupom foi comprado com sucesso',
+      'user/new-purchase',
+      { name, cupomName, partnerName },
+    );
+  }
 }
