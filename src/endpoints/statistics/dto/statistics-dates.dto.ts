eb9dfaf85e5c0c9cfed5e7665c6613191822d34f
Ricardo Monteiro <rmonteiro@marlin.com.br>
2024-03-10 15:50:04 -0300

    REC-134 Adição da doc do swagger

--- src/endpoints/statistics/dto/statistics-dates.dto.ts
+++ src/endpoints/statistics/dto/statistics-dates.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class StatisticsDatesDto {
+  @ApiProperty()
   mesInicial: number;
+
+  @ApiProperty()
   anoInicial: number;
+
+  @ApiProperty()
   mesFinal?: number;
+
+  @ApiProperty()
   anoFinal?: number;
 }
