96344f2f357625d1a227a6cdb3f8897bb54584c2
Ricardo Monteiro <rmonteiro@marlin.com.br>
2024-03-10 15:50:23 -0300

    Criação do header

--- src/shared/modules/pdf/pdf.service.ts
+++ src/shared/modules/pdf/pdf.service.ts
@@ -3,25 +3,65 @@ import * as PDFDocument from 'pdfkit';
 
 @Injectable()
 export class PdfService {
-  async generate(): Promise<Buffer> {
-    const pdfBuffer: Buffer = await new Promise((resolve) => {
-      const doc = new PDFDocument({
-        size: 'A4',
-        bufferPages: true,
-      });
+  private doc: PDFKit.PDFDocument;
 
-      // customize your PDF document
-      doc.text('hello world', 100, 50);
-      doc.end();
-
-      const buffer = [];
-      doc.on('data', buffer.push.bind(buffer));
-      doc.on('end', () => {
-        const data = Buffer.concat(buffer);
-        resolve(data);
-      });
+  create() {
+    this.doc = new PDFDocument({
+      size: 'A4',
+      bufferPages: true,
+      margins: {
+        top: 20,
+        bottom: 20,
+        left: 20,
+        right: 20,
+      },
     });
+  }
 
-    return pdfBuffer;
+  lineSeparator(y: number) {
+    this.doc
+      .moveDown()
+      .lineCap('butt')
+      .moveTo(20, y)
+      .lineTo(575, y)
+      .lineWidth(0.5)
+      .stroke();
+  }
+
+  addTitle(title: string) {
+    this.doc.fontSize(24).text(title);
+  }
+
+  addPeriod(initialDate: Date, finalDate: Date) {
+    this.doc
+      .fontSize(11)
+      .text(
+        `Período: ${
+          initialDate.getMonth() + 1
+        }/${initialDate.getFullYear()} - ${
+          finalDate.getMonth() + 1
+        }/${finalDate.getFullYear()}`,
+      );
+
+    this.lineSeparator(65);
+  }
+
+  async generate(): Promise<Buffer | null> {
+    if (this.doc) {
+      const pdfBuffer: Buffer = await new Promise((resolve) => {
+        // customize your PDF document
+        this.doc.end();
+
+        const buffer = [];
+        this.doc.on('data', buffer.push.bind(buffer));
+        this.doc.on('end', () => {
+          const data = Buffer.concat(buffer);
+          resolve(data);
+        });
+      });
+
+      return pdfBuffer;
+    }
+    return null;
   }
 }
